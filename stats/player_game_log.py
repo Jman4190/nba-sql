272c0d2e3f9c23311ccadb06f425705d03d2a57f
Matthew Pope <matt.leonard.pope@gmail.com>
2021-02-27 16:10:42 -0800

    untested PlayerGameLog

--- stats/player_game_log.py
+++ stats/player_game_log.py
@@ -42,68 +42,36 @@ class PlayerGameLog:
             new_row = {
                 'season_id': row[0],
                 'player_id': row[1],
-                'player_name = CharField(null=True) # TODO: Remove
-                'team_id = IntegerField(null=False)
-                'team_abbreviation = CharField(null=True)
-                'team_name = CharField(null=True)
-                'game_id = CharField(null=True)
-                'game_date = CharField(null=True)
-                'matchup = CharField(null=True)
-                'wl = FloatField(null=True)
-                'min = FloatField(null=True)
-                'fgm = FloatField(null=True)
-                'fga = FloatField(null=True)
-                'fg_pct = FloatField(null=True)
-                'fg3m = FloatField(null=True)
-                'fg3a = FloatField(null=True)
-                'fg3_pct = FloatField(null=True)
-                'ftm = FloatField(null=True)
-                'fta = FloatField(null=True)
-                'ft_pct = FloatField(null=True)
-                'oreb = FloatField(null=True)
-                'dreb = FloatField(null=True)
-                'reb = FloatField(null=True)
-                'ast = FloatField(null=True)
-                'tov = FloatField(null=True)
-                'stl = FloatField(null=True)
-                'blk = FloatField(null=True)
-                'blka = FloatField(null=True)
-                'pf = FloatField(null=True)
-                'pfd = FloatField(null=True)
-                'pts = FloatField(null=True)
-                'plus_minus = FloatField(null=True)
-                'nba_fantasy_pts = FloatField(null=True)
-                'dd2 = FloatField(null=True)
-                'td3 = FloatField(null=True)
-                'gp_rank = FloatField(null=True)
-                'w_rank = FloatField(null=True)
-                'l_rank = FloatField(null=True)
-                'w_pct_rank = FloatField(null=True)
-                'min_rank = FloatField(null=True)
-                'fgm_rank = FloatField(null=True)
-                'fga_rank = FloatField(null=True)
-                'fg_pct_rank = FloatField(null=True)
-                'fg3m_rank = FloatField(null=True)
-                'fg3a_rank = FloatField(null=True)
-                'fg3_pct_rank = FloatField(null=True)
-                'ftm_rank = FloatField(null=True)
-                'fta_rank = FloatField(null=True)
-                'ft_pct_rank = FloatField(null=True)
-                'oreb_rank = FloatField(null=True)
-                'dreb_rank = FloatField(null=True)
-                'reb_rank = FloatField(null=True)
-                'ast_rank = FloatField(null=True)
-                'tov_rank = FloatField(null=True)
-                'stl_rank = FloatField(null=True)
-                'blk_rank = FloatField(null=True)
-                'blka_rank = FloatField(null=True)
-                'pf_rank = FloatField(null=True)
-                'pfd_rank = FloatField(null=True)
-                'pts_rank = FloatField(null=True)
-                'plus_minus_rank = FloatField(null=True)
-                'nba_fantasy_pts_rank = FloatField(null=True)
-                'dd2_rank = FloatField(null=True)
-                'td3_rank = FloatField(null=True)
+                'team_id': row[3],
+                'game_id': row[6],
+                'game_date': row[7],
+                'matchup': row[8],
+                'wl': row[9],
+                'min': row[10]
+                'fgm': row[11],
+                'fga': row[12],
+                'fg_pct': row[13],
+                'fg3m': row[14],
+                'fg3a': row[15],
+                'fg3_pct': row[16],
+                'ftm': row[17],
+                'fta': row[18],
+                'ft_pct': row[19],
+                'oreb': row[20],
+                'dreb': row[21],
+                'reb': row[22],
+                'ast': row[23],
+                'tov': row[24],
+                'stl': row[25],
+                'blk': row[26],
+                'blka': row[27],
+                'pf': row[28],
+                'pfd': row[29],
+                'pts': row[30],
+                'plus_minus': row[31],
+                'nba_fantasy_pts': row[32],
+                'dd2': row[33],
+                'td3': row[34]
             }
             rows.apppend(new_row)
 
